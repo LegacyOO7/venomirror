0e85fb6318be88b61322ebfb187b2dc10ee2f35a
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-17 23:51:43 +0300

    Update gdriveTools.py

--- bot/helper/mirror_utils/upload_utils/gdriveTools.py
+++ bot/helper/mirror_utils/upload_utils/gdriveTools.py
@@ -377,24 +377,24 @@ class GoogleDriveHelper:
                     LOGGER.info("Deleting cloned data from Drive...")
                     msg = self.deletefile(durl)
                     LOGGER.info(f"{msg}")
-                    return "Your Clone has been Stopped and Cloned data has been deleted!", "cancelled"
-                msg += f'<b>📂 Filename : </b><code>{meta.get("name")}</code>\n\n<b>💽 Size : {get_readable_file_size(self.transferred_size)}</b>'
-                msg += f'\n<b>📦 Type : Folder</b>'
-                msg += f'\n\n<b>© Powerd By : WhitE_DeviL09</b>'
+                    return "your clone has been stopped and cloned data has been deleted!", "cancelled"
+                msg += f'<b>☞ 📂Filename : </b><code>{meta.get("name")}</code>\n<b>Size: </b><code>{get_readable_file_size(self.transferred_size)}</code>'
+                msg += f'\n<b>☞ 🌀Type : </b><code>Folder</code>'
+                msg += f'\n<b>☞ Powerd by : @AT_BOTs</b>'
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={durl}&format=text').text
-                    buttons.buildbutton("🔗 Drive Link 🔗", surl)
+                    buttons.buildbutton("🌠 Drive Link 🌠", surl)
                 else:
-                    buttons.buildbutton("🔗 Drive Link 🔗", durl)
+                    buttons.buildbutton("🌠 Drive Link 🌠", durl)
                 if INDEX_URL is not None:
                     url_path = requests.utils.quote(f'{meta.get("name")}')
                     url = f'{INDEX_URL}/{url_path}/'
                     if SHORTENER is not None and SHORTENER_API is not None:
                         siurl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={url}&format=text').text
-                        buttons.buildbutton("🧲 Index Link 🧲", siurl)
+                        buttons.buildbutton("☄️ Index Link ☄️", siurl)
                     else:
-                        buttons.buildbutton("🧲 Index Link 🧲", url)
+                        buttons.buildbutton("☄️ Index Link ☄️", url)
                 if BUTTON_FOUR_NAME is not None and BUTTON_FOUR_URL is not None:
                     buttons.buildbutton(f"{BUTTON_FOUR_NAME}", f"{BUTTON_FOUR_URL}")
                 if BUTTON_FIVE_NAME is not None and BUTTON_FIVE_URL is not None:
@@ -403,21 +403,22 @@ class GoogleDriveHelper:
                     buttons.buildbutton(f"{BUTTON_SIX_NAME}", f"{BUTTON_SIX_URL}")
             else:
                 file = self.copyFile(meta.get('id'), parent_id)
-                msg += f'<b>📂 Filename : </b><code>{file.get("name")}</code>'
+                msg += f'<b>☞ 📂Filename : </b><code>{file.get("name")}</code>'
                 durl = self.__G_DRIVE_BASE_DOWNLOAD_URL.format(file.get("id"))
                 buttons = button_build.ButtonMaker()
                 if SHORTENER is not None and SHORTENER_API is not None:
                     surl = requests.get(f'https://{SHORTENER}/api?api={SHORTENER_API}&url={durl}&format=text').text
-                    buttons.buildbutton("🔗 Drive Link 🔗", surl)
+                    buttons.buildbutton("🌠 Drive Link 🌠", surl)
                 else:
-                    buttons.buildbutton("🔗 Drive Link 🔗", durl)
+                    buttons.buildbutton("🌠 Drive Link 🌠", durl)
                 try:
                     typeee = file.get('mimeType')
                 except:
                     typeee = 'File' 
                 try:
-                    msg += f'\n<b>📦 Size : {get_readable_file_size(int(meta.get("size")))}</b>'
-                    msg += f'\n<b>© Powerd By : WhitE_DeviL09</b> '
+                    msg += f'\n<b>☞ 📦Size : </b><code>{get_readable_file_size(int(meta.get("size")))}</code>'
+                    msg += f'\n<b>☞ 🗳Powerd by : @AT_BOTS</b> '
+            
                 except TypeError:
                     pass
                 if INDEX_URL is not None:
