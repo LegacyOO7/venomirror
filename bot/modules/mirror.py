68e894cbd10d4bfa327158be83b9fdb5553eba9c
DREAM CATCHER <63229691+DreamCatcher96@users.noreply.github.com>
2022-01-18 11:06:35 +0300

    Update mirror.py

--- bot/modules/mirror.py
+++ bot/modules/mirror.py
@@ -138,7 +138,7 @@ class MirrorListener(listeners.MirrorListeners):
             uname = f"@{self.message.from_user.username}"
         else:
             uname = f'<a href="tg://user?id={self.message.from_user.id}">{self.message.from_user.first_name}</a>'
-        msg = f"{uname} your download has been stopped due to: {error}"
+        msg = f"<b>📤 {uname} Your Download has Been Stopped Due to : {error} 🤒</b>"
         sendMessage(msg, self.bot, self.update)
         if count == 0:
             self.clean()
